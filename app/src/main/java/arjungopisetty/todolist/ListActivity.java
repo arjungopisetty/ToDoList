eb590e89f5faf5bdd4d960607a33edf23f0cade4
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2014-10-10 14:50:12 -0500

    Added the ListView and a new Activity for adding new task to the ListView. Basic framework only; functionality is still in development.

--- app/src/main/java/arjungopisetty/todolist/ListActivity.java
+++ app/src/main/java/arjungopisetty/todolist/ListActivity.java
@@ -1,17 +1,44 @@
 package arjungopisetty.todolist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class ListActivity extends Activity {
+
+    private ListView mList;
+
+    private ArrayList<String> mData;
+    private ArrayAdapter<String> mArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        mList = (ListView) findViewById(R.id.listView);
+        mData = new ArrayList<String>();
+        mData.add("Michael Wang");
+
+        mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mData);
+        mList.setAdapter(mArrayAdapter);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Edward is Cool", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
@@ -30,7 +57,12 @@ public class ListActivity extends Activity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.newTask) {
+            Intent intent = new Intent(this, NewTaskActivity.class);
+            startActivityForResult(intent, 1);
+            startActivity(intent);
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
